ec27d5398367ca64e442af1f1ed0009be879a76d
0xGautham <saigautam@webmobsoft.com>
2024-02-22 10:59:00 +0530

    first commit

---
+++ scripts/init.ts
@@ -0,0 +1,32 @@
+import fs from "fs/promises";
+import path from "path";
+import prettier from "prettier";
+import { ethers } from "ethers";
+
+const INIT_CONFIG = {
+  rpcUrl: "https://api.stackup.sh/v1/node/1557ce1f5ca196324a146bce7fb3fdafc023df3e2f4c76dfdd6cd8064251ff4b",
+  signingKey: new ethers.Wallet(ethers.utils.randomBytes(32)).privateKey,
+  entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+  simpleAccountFactory: "0x9406Cc6185a346906296840746125a0E44976454",
+  paymaster: {
+    rpcUrl: "https://api.stackup.sh/v1/paymaster/1557ce1f5ca196324a146bce7fb3fdafc023df3e2f4c76dfdd6cd8064251ff4b",
+    context: {
+      "type": "payg"
+    },
+  },
+};
+const CONFIG_PATH = path.resolve(__dirname, "../config.json");
+
+async function main() {
+  return fs.writeFile(
+    CONFIG_PATH,
+    prettier.format(JSON.stringify(INIT_CONFIG, null, 2), { parser: "json" })
+  );
+}
+
+main()
+  .then(() => console.log(`Config written to ${CONFIG_PATH}`))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
